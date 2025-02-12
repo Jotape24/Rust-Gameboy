c6293939be41485074d967c3432b039a0efd3831
Jotape <jose-pablo1@hotmail.com>
2025-02-11 23:03:28 -0300

    add: getters and setters for registers

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -2,7 +2,7 @@ use crate::cpu::registers::flag_register::FlagsRegister;
 
 mod flag_register;
 
-struct Registers {
+pub struct Registers {
     a: u8,
     b: u8,
     c: u8,
@@ -53,7 +53,55 @@ impl Registers {
         (self.h as u16) << 8
             | (self.l as u16)
     }
+}
 
 
+impl Registers {
+    fn set_a(&mut self, value: u8) {
+        self.a = value;
+    }
 
+    pub fn get_a(&self) -> u8 {self.a}
+
+    fn set_b(&mut self, value: u8) {
+        self.b = value;
+    }
+
+    pub fn get_b(&self) -> u8 {self.b}
+
+    fn set_c(&mut self, value: u8) {
+        self.c = value;
+    }
+
+    pub fn get_c(&self) -> u8 {self.c}
+
+    fn set_d(&mut self, value: u8) {
+        self.d = value;
+    }
+
+    pub fn get_d(&self) -> u8 {self.d}
+
+    fn set_e(&mut self, value: u8) {
+        self.e = value;
+    }
+
+    pub fn get_e(&self) -> u8 {self.e}
+
+    fn set_f(&mut self, value: u8) {
+        self.f = FlagsRegister::from(value);
+    }
+
+    pub fn get_f(&self) -> u8 {u8::from(self.f.clone())}
+
+    fn set_h(&mut self, value: u8) {
+        self.h = value;
+    }
+
+    pub fn get_h(&self) -> u8 {self.h}
+
+    fn set_l(&mut self, value: u8) {
+        self.l = value;
+    }
+
+    pub fn get_l(&self) -> u8 {self.l}
 }
