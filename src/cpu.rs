b6c8e162f54e7306f65b38f6ff07ddda5a2b138e
Jotape <jose-pablo1@hotmail.com>
2025-02-12 23:11:17 -0300

    feat: execute SUB implemented and tested

--- src/cpu.rs
+++ src/cpu.rs
@@ -92,6 +92,38 @@ impl CPU {
                     },
                 }
             }
+            Instruction::SUB(target) => {
+                match target {
+                    ArithmeticTarget::A => {
+                        let val = self.registers.get_a();
+                        self.sub(val);
+                    },
+                    ArithmeticTarget::B => {
+                        let val = self.registers.get_b();
+                        self.sub(val);
+                    },
+                    ArithmeticTarget::C => {
+                        let val = self.registers.get_c();
+                        self.sub(val);
+                    },
+                    ArithmeticTarget::D => {
+                        let val = self.registers.get_d();
+                        self.sub(val);
+                    },
+                    ArithmeticTarget::E => {
+                        let val = self.registers.get_e();
+                        self.sub(val);
+                    },
+                    ArithmeticTarget::H => {
+                        let val = self.registers.get_h();
+                        self.sub(val);
+                    },
+                    ArithmeticTarget::L => {
+                        let val = self.registers.get_l();
+                        self.sub(val);
+                    },
+                }
+            }
             _ => {}
         }
     }
@@ -129,19 +161,36 @@ impl CPU {
         self.registers.set_hl(new_value);
     }
 
+    // adds a value + carry to the A register
     fn add_carry(&mut self, value: u8) {
-        let (new_value, did_overflow) = self.registers.get_a().overflowing_add(value);
+        let carry_val = u8::from(self.registers.get_f().get_carry()); // Obtener el carry actual
+
+        let (partial_sum, carry1) = self.registers.get_a().overflowing_add(value);
+        let (new_value, carry2) = partial_sum.overflowing_add(carry_val);
+
         let zero = new_value == 0;
         let subtract = false;
-        let (carry, carry_val) = (did_overflow, u8::from(did_overflow));
-        // Half Carry is set if adding the lower nibbles of the value and register A
-        // together result in a value bigger than 0xF. If the result is larger than 0xF
-        // than the addition caused a carry from the lower nibble to the upper nibble.
-        let half_carry = (self.registers.get_a() & 0xF) + (value & 0xF) > 0xF;
+        let carry = carry1 || carry2;
+
+        let half_carry = ((self.registers.get_a() & 0xF) + (value & 0xF) + carry_val) > 0xF;
 
         let new_f = FlagsRegister::new(zero, subtract, half_carry, carry);
         self.registers.set_f(new_f);
-        self.registers.set_a(new_value + carry_val);
+        self.registers.set_a(new_value);
+    }
+
+
+    fn sub(&mut self, value: u8) {
+        let (new_value, did_overflow) = self.registers.get_a().overflowing_sub(value);
+        let zero = new_value == 0;
+        let subtract = true;
+        let carry = did_overflow;
+        // if the lower nibble of value is bigger than the lower nibble of A
+        let half_carry = (self.registers.get_a() & 0xF) < (value & 0xF);
+
+        let new_f = FlagsRegister::new(zero, subtract, half_carry, carry);
+        self.registers.set_f(new_f);
+        self.registers.set_a(new_value);
     }
 }
 
@@ -191,7 +240,6 @@ mod cpu_test {
 
     #[test]
     fn test_execute_add_carry () {
-
         let f = FlagsRegister::new(false, true, true, true);
 
         let register = Registers::new(0b1111_1111, 2, 3, 0b0000_0010, 5, f, 6, 7);
@@ -205,7 +253,22 @@ mod cpu_test {
         assert_eq!(cpu.registers.get_f(), expected_f);
 
         assert_eq!(cpu.registers.get_a(), 0b0000_0010);
+    }
 
+    #[test]
+    fn test_execute_sub () {
+        let f = FlagsRegister::new(false, true, true, true);
 
+        let register = Registers::new(0b1111_0001, 2, 3, 0b0000_0010, 5, f, 6, 7);
+
+        let mut cpu = CPU { registers: register };
+
+        cpu.execute(Instruction::SUB(ArithmeticTarget::D));
+
+        let expected_f = FlagsRegister::new(false, true, true, false);
+
+        assert_eq!(cpu.registers.get_f(), expected_f);
+
+        assert_eq!(cpu.registers.get_a(), 0b01110_1111);
     }
 }
