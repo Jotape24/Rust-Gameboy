71ce62f77f133a1d0330956092b3be7e593395ea
Jotape <jose-pablo1@hotmail.com>
2025-02-12 22:44:33 -0300

    feat: execute ADC implemented and tested

--- src/cpu.rs
+++ src/cpu.rs
@@ -60,6 +60,38 @@ impl CPU {
                     }
                 }
             }
+            Instruction::ADC(target) => {
+                match target {
+                    ArithmeticTarget::A => {
+                        let val = self.registers.get_a();
+                        self.add_carry(val);
+                    },
+                    ArithmeticTarget::B => {
+                        let val = self.registers.get_b();
+                        self.add_carry(val);
+                    },
+                    ArithmeticTarget::C => {
+                        let val = self.registers.get_c();
+                        self.add_carry(val);
+                    },
+                    ArithmeticTarget::D => {
+                        let val = self.registers.get_d();
+                        self.add_carry(val);
+                    },
+                    ArithmeticTarget::E => {
+                        let val = self.registers.get_e();
+                        self.add_carry(val);
+                    },
+                    ArithmeticTarget::H => {
+                        let val = self.registers.get_h();
+                        self.add_carry(val);
+                    },
+                    ArithmeticTarget::L => {
+                        let val = self.registers.get_l();
+                        self.add_carry(val);
+                    },
+                }
+            }
             _ => {}
         }
     }
@@ -80,6 +112,7 @@ impl CPU {
         self.registers.set_a(new_value);
     }
 
+    // adds a value to the HL register
     fn add_hl(&mut self,value: u16) {
         let (new_value, did_overflow) = self.registers.get_hl().overflowing_add(value);
         let zero = self.registers.get_f().get_zero();
@@ -87,13 +120,28 @@ impl CPU {
         let carry = did_overflow;
 
         // Half Carry is set if adding the lower nibbles of the value and register HL
-        // together result in a value bigger than 0xFFF. If the result is larger than 0xFFF
-        // than the addition caused a carry from the lower nibble to the upper nibble.
+        // together result in a value bigger than 0xFFF. is the pass from the lower nibble from the
+        // higher byte to the upper nibble
         let half_carry = (self.registers.get_hl() & 0xFFF) + (value & 0xFFF) > 0xFFF;
 
         let new_f = FlagsRegister::new(zero, subtract, half_carry, carry);
         self.registers.set_f(new_f);
         self.registers.set_hl(new_value);
+    }
+
+    fn add_carry(&mut self, value: u8) {
+        let (new_value, did_overflow) = self.registers.get_a().overflowing_add(value);
+        let zero = new_value == 0;
+        let subtract = false;
+        let (carry, carry_val) = (did_overflow, u8::from(did_overflow));
+        // Half Carry is set if adding the lower nibbles of the value and register A
+        // together result in a value bigger than 0xF. If the result is larger than 0xF
+        // than the addition caused a carry from the lower nibble to the upper nibble.
+        let half_carry = (self.registers.get_a() & 0xF) + (value & 0xF) > 0xF;
+
+        let new_f = FlagsRegister::new(zero, subtract, half_carry, carry);
+        self.registers.set_f(new_f);
+        self.registers.set_a(new_value + carry_val);
     }
 }
 
@@ -135,8 +183,29 @@ mod cpu_test {
 
         assert_eq!(cpu.registers.get_hl(), 0b0000000000000110);
 
-        let expected_f = FlagsRegister::new(false, false, false, true);
+        let expected_f = FlagsRegister::new(false, false, true, true);
 
-        // assert_eq!(cpu.registers.get_f(), expected_f);
+        assert_eq!(cpu.registers.get_f(), expected_f);
+    }
+
+
+    #[test]
+    fn test_execute_add_carry () {
+
+        let f = FlagsRegister::new(false, true, true, true);
+
+        let register = Registers::new(0b1111_1111, 2, 3, 0b0000_0010, 5, f, 6, 7);
+
+        let mut cpu = CPU { registers: register };
+
+        cpu.execute(Instruction::ADC(ArithmeticTarget::D));
+
+        let expected_f = FlagsRegister::new(false, false, true, true);
+
+        assert_eq!(cpu.registers.get_f(), expected_f);
+
+        assert_eq!(cpu.registers.get_a(), 0b0000_0010);
+
+
     }
 }
